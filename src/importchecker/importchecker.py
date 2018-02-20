43d512bccf0727261e34002f4f79a636b68a9d43
Jan-Wijbrand Kolman <jkolman@minddistrict.com>
2018-02-20 14:24:45 +0100

    the name of the utility

--- src/importchecker/importchecker.py
+++ src/importchecker/importchecker.py
@@ -11,7 +11,7 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ##############################################################################
-"""Import checker
+"""Importchecker
 
 This utility finds unused imports in Python modules.  Its output is
 grep-like and thus emacs-friendly.
