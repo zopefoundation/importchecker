09407ded47718c21178684570e6d00bb336a6337
Jan-Wijbrand Kolman <jkolman@minddistrict.com>
2018-02-22 11:18:28 +0100

    Back to development: 2.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 from setuptools import setup, find_packages
 
 
-version = '2.0'
+version = '2.1.dev0'
 description = "Importchecker finds unused imports in Python modules."
 test_requirements = [
     'zope.testrunner'
